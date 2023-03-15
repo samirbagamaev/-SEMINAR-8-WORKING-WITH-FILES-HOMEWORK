1b92ea4eda7db3c6cabda12f03f7ce5bc04c642b
Samir Bagamaev <05ru4315@gmail.com>
2023-03-15 18:18:46 +0300

    work

---
+++ WORK_38/interface.py
@@ -0,0 +1,18 @@
+while True:
+    choise = int(input('Введите нужное действие: \n 1 - Добавить в справочник \
+        \n 2 - Вывести всех \n 3 - Поиск по фамилии \n 4 - Удалить ифнормацию \
+        \n 5 - Выход \n' ))
+    match choise: 
+        case 1: 
+            import input1
+            input1.Input(input('Введите имя: '), input('Введите номер: '))
+        case 2: 
+            from output import OutputAll
+            OutputAll()
+        case 3:
+            from output import Search
+            Search(input('Введите фамилию: '))
+        case 4:
+            from output import delete
+            delete(input('Введите имя или фамилию: '))
+        case 5: break
